967d1b077fd61baf621f3ed29b581b5116705c4d
Siddhant Sharma <79889236+siddhant385@users.noreply.github.com>
2022-06-26 16:08:25 +0530

    Create requirements.bat

---
+++ GIFS/requirements.bat
@@ -0,0 +1,15 @@
+echo off
+
+pip3 install pytelegrambotapi
+pip3 install pycryptodome
+pip3 install pyperclip
+pip3 install keyboard
+pip3 install pyttsx3
+pip3 install pywin32
+pip3 install pynput
+pip3 install mss
+
+pip3 install pyinstaller
+
+:cmd
+pause null
